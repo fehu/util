8f57835e3290fa74f7c0ea8fe2a3ba941fc170b2
fehu <lord.fehu@gmail.com>
2015-01-23 17:12:19 -0600

    NotNegative fix;

--- src/main/scala/feh/util/NotNegative.scala
+++ src/main/scala/feh/util/NotNegative.scala
@@ -5,7 +5,7 @@ import scala.language.implicitConversions
 class NotNegative[N: Numeric] protected (val value: N)
 object NotNegative{
   implicit def apply[N](n: N)(implicit num: Numeric[N]): NotNegative[N] = {
-    assert(num.compare(n, num.zero) < 0, s"$n is negative")
+    assert(num.compare(n, num.zero) >= 0, s"$n is negative")
     new NotNegative[N](n)
   }
 
